addfdeedada23e8451f51b92d71fdb48a3283f15
Bahadir Sengun <bahadirsngun@gmail.com>
2024-08-01 23:34:38 +0300

    ikinci ödev yapıldı. To Do List.

---
+++ SwiftUiTutoriaal/StateSample/ToDoListSample.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListSample.swift
+//  SwiftUiTutoriaal
+//
+//  Created by Bahadır Sengun on 1.08.2024.
+//
+
+import SwiftUI
+
+struct ToDoListSample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListSample()
+}
