47bba5492d2b2d89cb75c041a746cf80d159c132
Bahadir Sengun <bahadirsngun@gmail.com>
2024-08-01 23:32:43 +0300

    birinci ödev. Celciusu Fahrenhayta çevirme.

--- SwiftUiTutoriaal/StateSample/CelciusSample.swift
+++ SwiftUiTutoriaal/StateSample/CelciusSample.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct CelciusSample: View {
+    //değişkenler
+    @State var celcius = "0"
+    @State var fahrenayt = "0"
+    var title = "Celcius to Fahrenheit"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        Text(title)
+            .padding()
+            .bold()
+            .font(.title)
+
+        HStack{
+            TextField("celcius", text:$celcius)
+                .padding()
+                .bold()
+                .frame(width: 150, height: 50)
+                .background(Color(.lightGray))
+                .cornerRadius(12)
+                .padding()
+                
+            
+            Button{
+                if let celcius1 = Double(celcius), let fahrenayt1 = Double(fahrenayt){
+                    fahrenayt = String(celcius1 * 1.8 + 32)
+                } else {
+                    fahrenayt = "Invalid Number"
+                }
+            }
+        label:{
+            Image(systemName: "arrowshape.turn.up.backward")
+        }
+            
+            Text("Fahrenayt: \(fahrenayt)")
+                .padding()
+            
+        }
+        Spacer()
+    
     }
 }
+    
+    #Preview {
+        CelciusSample()
+    }
 
-#Preview {
-    CelciusSample()
-}
+
