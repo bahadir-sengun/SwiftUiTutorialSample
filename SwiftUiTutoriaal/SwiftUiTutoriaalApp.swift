36a80958018c9c4423056a63e52f57f3cad7637c
Bahadir Sengun <bahadirsngun@gmail.com>
2024-08-01 23:30:43 +0300

    Initial Commit

---
+++ SwiftUiTutoriaal/SwiftUiTutoriaalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTutoriaalApp.swift
+//  SwiftUiTutoriaal
+//
+//  Created by Bahadır Sengun on 1.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTutoriaalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
